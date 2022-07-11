b9d0a6c182b6230751ffe917f8cdf90a4eefdf8d
jamesmwu <94282326+jamesmwu@users.noreply.github.com>
2022-07-11 15:25:18 -0700

    Updated Contact buttons, Projects info, added Resume, fixed minor bugs

--- app.js
+++ app.js
@@ -14,6 +14,27 @@ function complete() {
   timer = null;
 }
 
+function cycle() {
+  //Enables random display
+  // var randOption = Math.floor(Math.random() * funFacts.length);
+
+  //Sequential display
+  if (i === funFacts.length) i = 0;
+
+  document.getElementById("fact").innerHTML = funFacts[i];
+  i++;
+}
+
+var i = 0;
+var funFacts = [
+  "Outside of programming, some of my hobbies include playing basketball, badminton, running, drawing, writing, and playing violin!",
+  "I'm red green colorblind!",
+  "I love public speaking, and have participated in mock trial as well as advanced two levels through the International Rotary 4-Way Test speech competition. I've even presented a Ted Talk before!",
+  "I once fought a coyote alone, bare-handed.",
+  "I'd really like to run a marathon or triathlon some day.",
+  "",
+];
+
 const text = document.querySelector(".james");
 const strText = text.textContent; //This gives actual text of class "james".
 const splitText = strText.split(""); //Returns array of each individual letter
