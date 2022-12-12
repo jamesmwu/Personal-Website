5a4fd44926b5b00e8a0639ccfec0d43834684bf4
jameswu <jamesw0462@gmail.com>
2022-12-12 13:56:37 -0800

    Foundations-- body, navigation bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Test</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Navigation */}
+      <div id='navigation'>
+        <h1>James Wu</h1>
+        <nav>
+          <Link className='navLinks' to='/'>
+            Home
+          </Link>
+          <Link className='navLinks' to='/experience'>
+            Experience
+          </Link>
+          <Link className='navLinks' to='/contact'>
+            Contact
+          </Link>
+        </nav>
+      </div>
+
+      {/* Routes */}
+      <div id='content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='contact' element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Experience() {
+  return (
+    <div>
+      <p>booga</p>
+    </div>
+  );
+}
+
+export default Experience;
