4f8de4992626749065a043b1e58d76d1fef57b92
jameswu <jamesw0462@gmail.com>
2023-08-26 11:57:37 -0700

    Added images and effects to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Collapsible from 'react-collapsible';
 import { aboutMe } from '../data/info';
 import '../styles/About.css';
+import Cactus from '../img/aboutPage/Cactus.png';
+import Deer from '../img/aboutPage/Deer.png';
+import Sunset from '../img/aboutPage/Sunset.png';
 
 function About() {
 	const [fadeIn, setFadeIn] = useState(false);
@@ -16,13 +19,24 @@ function About() {
 
 	return (
 		<div id='aboutPage' className={fadeIn ? 'fade-in' : 'fade-out'}>
+			<div className='imageContainer'>
+				<div className='imageWrapper left'>
+					<img id='sunset' src={Sunset} alt='Me during a sunset' />
+				</div>
+				<div className='imageWrapper larger'>
+					<img id='cactus' src={Cactus} alt='Me posing next to a cactus' />
+				</div>
+				<div className='imageWrapper right'>
+					<img id='deer' src={Deer} alt='Me holding a deer head over my face' />
+				</div>
+			</div>
+
 			<div className='aboutBio'>
 				<h1 id='heroText'>Hello! I'm James.</h1>
 				<p id='heroDesc'>
 					👋 Hello! I'm James, a CS Dean's Honors list student at UCLA with a
-					passion for web and mobile development. As an aspiring developer, I
-					thrive on creating innovative solutions and exploring new
-					technologies.
+					passion for software development. As an aspiring developer, I thrive
+					on creating innovative solutions and exploring new technologies.
 				</p>
 				<div className='subSection'>
 					{aboutMe.map((info, index) => (
